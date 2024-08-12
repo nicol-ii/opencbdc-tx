f4e2415d9c2860577ef698365281f749d904e22c
Nicole Li <134007984+nicol-ii@users.noreply.github.com>
2024-08-12 16:15:49 -0400

    wip
    Signed-off-by: Nicole Li <134007984+nicol-ii@users.noreply.github.com>

--- scripts/contracts/contract_CAD.lua
+++ scripts/contracts/contract_CAD.lua
@@ -1,14 +1,14 @@
 function contract(param)
     mode = param[1]
     if mode == "pay" then
-        return true
+        error("not implemented yet")
     elseif mode == "earmark" then
         _, name, amount = table.unpack(param)
         value = trylock(name)
         if value >= amount then
-            return true
+            return {[name]=trylock(name)-amount}
         else
-            return false
+            error("insufficient funds")
         end
     elseif mode == "release" then
         _, name, amount = table.unpack(param)

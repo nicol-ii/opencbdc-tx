5be37cddbdee0c30379196f80f3eece2c713b5a4
nicol-ii <nicoleli20149@gmail.com>
2025-02-25 15:22:25 -0500

    adds useful comment to make_hash
    Signed-off-by: nicol-ii <nicoleli20149@gmail.com>

--- src/parsec/agent/runners/lua/impl.cpp
+++ src/parsec/agent/runners/lua/impl.cpp
@@ -321,7 +321,7 @@ namespace cbdc::parsec::agent::runner {
         cbdc::hash_t computed_hash{};
         sha.Finalize(computed_hash.data());
         lua_pushlstring(L, reinterpret_cast<char*>(computed_hash.data()), sz);
-        return 1;
+        return 1; // function returns 1 stack element
     }
 
 }
